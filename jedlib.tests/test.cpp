6f1b63748b9d64a00c37a967e5893be637acaa50
janm31415 <Jan.Maes@materialise.be>
2026-09-14 15:39:53 +0200

    mem leak tracking

--- jedlib.tests/test.cpp
+++ jedlib.tests/test.cpp
@@ -3,8 +3,34 @@
 #include "test_edit.h"
 #include <ctime>
 
+#if defined(MEMORY_LEAK_TRACKING) && defined(_MSC_VER)
+#ifndef NDEBUG
+#define MEMORY_LEAK_TRACKING_MSVC
+#endif
+#endif
+
+void init_debug()
+  {
+#if defined(MEMORY_LEAK_TRACKING_MSVC)
+  int flags = _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG);
+  _CrtSetDbgFlag(flags | _CRTDBG_ALLOC_MEM_DF);
+#endif
+  }
+
+void close_debug()
+  {
+#if defined(MEMORY_LEAK_TRACKING_MSVC)
+  int flags = _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG);
+  flags &= ~_CRTDBG_DELAY_FREE_MEM_DF;
+  flags |= _CRTDBG_LEAK_CHECK_DF;
+  _CrtSetDbgFlag(flags);
+#endif
+  }
+
 int main(int /*argc*/, const char* /*argv*/[])
   {
+  init_debug();
+
   InitTestEngine();
 
   auto tic = std::clock();
@@ -21,5 +47,7 @@ int main(int /*argc*/, const char* /*argv*/[])
     TEST_OUTPUT_LINE("FAILURE: %d out of %d tests failed (%d failures).", testing_fails, testing_success+testing_fails, testing_fails);
     }
   TEST_OUTPUT_LINE("Test time: %f seconds.", (double)(toc - tic)/(double)CLOCKS_PER_SEC);
-  return CloseTestEngine(true);
+  int res = CloseTestEngine(true);
+  close_debug();
+  return res;
   }
